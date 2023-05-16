3100d865bf438e42df0f2cc97314340ce77fadd4
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-05-16 18:57:37 +0300

    ft: change status bar color and initialise splash.

--- app/src/main/java/com/example/incode/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/incode/fragments/SplashFragment.kt
@@ -1,12 +1,25 @@
 package com.example.incode.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.example.incode.R
 
 class SplashFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //setting the status bar color
+        val window = requireActivity().window
+        window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+
+        //initialising the splash
+        Handler(Looper.getMainLooper()).postDelayed({
+            NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 3000)
 
     }
 }
