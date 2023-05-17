4fd3bcfc1751a5dfaa16ce84577ff479b001387f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-05-17 16:54:44 +0300

    ft: add a click listener to the submit button and initialise a view binding instance.

--- app/src/main/java/com/example/incode/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/incode/fragments/LoginFragment.kt
@@ -1,6 +1,28 @@
 package com.example.incode.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.incode.R
+import com.example.incode.databinding.FragmentLoginBinding
 
-class LoginFragment: Fragment() {
+class LoginFragment: Fragment(), View.OnClickListener {
+    private lateinit var bind: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        bind = FragmentLoginBinding.inflate(inflater)
+        bind.submit.setOnClickListener(this::onClick)
+        return bind.root
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 == bind.submit){
+            p0.findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            //NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
 }
