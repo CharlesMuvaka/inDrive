12be1392f2d2108cdab31223f29fce6f72dda6b0
CharlesMuvaka <muvakacharles@gmail.com>
2023-06-22 16:40:36 +0300

    bind the recycler view layout with place data.

--- app/src/main/java/com/example/incode/adapters/PlacesAdapter.kt
+++ app/src/main/java/com/example/incode/adapters/PlacesAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.example.incode.databinding.FragmentPlaceBinding
 import com.example.incode.databinding.PlaceRecBinding
 import com.example.incode.databinding.RightPlaceRecBinding
 import com.example.incode.models.Place
@@ -25,28 +26,17 @@ class PlacesAdapter: RecyclerView.Adapter<PlacesAdapter.MyHolder>() {
     }
 
     val asyncList = AsyncListDiffer(this, listDiffer)
-    inner class MyHolder(private val bind: ViewBinding): RecyclerView.ViewHolder(bind.root){
+    inner class MyHolder(private val bind: FragmentPlaceBinding): RecyclerView.ViewHolder(bind.root){
         fun setData(place: Place){
-//            if (layoutPosition == 0){
-//                (bind as PlaceRecBinding).name.text = place.title
-//                bind.description.text = "${place.description!!.substring(0, 50)}...."
-//            }else{
-//                (bind as RightPlaceRecBinding).name.text = place.title
-//                bind.description.text = "${place.description!!.substring(0, 50)}...."
-//            }
+            bind.placeName.text = place.title
+            bind.placeImage.setImageResource(place.image!!)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = if(layoutPosition != 0){
-        MyHolder(
-            PlaceRecBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        )
-    }else{
-        MyHolder(
-            RightPlaceRecBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyHolder(
+        FragmentPlaceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
 
-    }
 
     override fun getItemCount() = asyncList.currentList.size
 
