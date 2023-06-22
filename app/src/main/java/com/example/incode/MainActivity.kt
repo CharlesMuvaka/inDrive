421cd89f6b4bc61486ed78c7355140bfe4debc67
CharlesMuvaka <muvakacharles@gmail.com>
2023-06-22 16:38:21 +0300

    call the onTransformationStartContainer to initiate views transformation.

--- app/src/main/java/com/example/incode/MainActivity.kt
+++ app/src/main/java/com/example/incode/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.incode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.incode.datab.Drivers
+import com.example.incode.datab.Places
+import com.example.incode.models.Driver
+import com.example.incode.models.Place
+import com.skydoves.transformationlayout.onTransformationStartContainer
 
 class MainActivity : AppCompatActivity() {
+    var places = ArrayList<Place>()
+    var drivers = ArrayList<Driver>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        onTransformationStartContainer() //initialising view transformation
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        places = Places().places
+        drivers = Drivers().drivers
     }
 }
