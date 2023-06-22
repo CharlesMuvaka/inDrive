86c5c5162bda9fee427e03508b4a90da54823928
CharlesMuvaka <muvakacharles@gmail.com>
2023-06-22 16:41:48 +0300

    define a blue print to hold the splash screen information.

--- app/src/main/java/com/example/incode/models/SplashData.kt
+++ app/src/main/java/com/example/incode/models/SplashData.kt
@@ -1,6 +1,8 @@
 package com.example.incode.models
 
-class SplashData(val title: String) {
-    val description: String? = null
-    val image: Int? = null
+import java.io.Serializable
+
+class SplashData(val title: String): Serializable {
+    var description: String? = null
+    var image: Int? = null
 }
