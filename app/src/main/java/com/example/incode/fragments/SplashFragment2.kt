4150b7605b8afe4bdf606d64d4346be90a553ae0
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-03 03:58:49 +0300

    add click listeners on the buttons

---
+++ app/src/main/java/com/example/incode/fragments/SplashFragment2.kt
@@ -0,0 +1,45 @@
+package com.example.incode.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.example.incode.R
+import com.example.incode.databinding.FragmentSplash2Binding
+
+class SplashFragment2: Fragment(), View.OnClickListener {
+    private lateinit var bind: FragmentSplash2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment2_to_splashFragment)
+        }, 5000)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        bind = FragmentSplash2Binding.inflate(inflater)
+
+        //setting the click listeners
+        bind.buttonNext.setOnClickListener(this::onClick)
+        bind.buttonSkip.setOnClickListener(this::onClick)
+
+        return bind.root
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 == bind.buttonNext){
+            NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment2_to_splashFragment)
+        }
+        if (p0 == bind.buttonSkip){
+            NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment2_to_loginFragment)
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/incode/fragments/SplashPagerFragment.kt
+++ app/src/main/java/com/example/incode/fragments/SplashPagerFragment.kt
@@ -1,32 +1,30 @@
 package com.example.incode.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.incode.R
 import com.example.incode.databinding.FragmentSplashPagerBinding
 import com.example.incode.models.SplashData
 
 class SplashPagerFragment: Fragment(), View.OnClickListener {
     private lateinit var bind: FragmentSplashPagerBinding
-    private var splashObject: SplashData? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            splashObject = it.getSerializable("splash") as SplashData
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            NavHostFragment.findNavController(this).navigate(R.id.action_splashPagerFragment_to_splashFragment2)
+        }, 5000)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bind = FragmentSplashPagerBinding.inflate(inflater)
-
-        splashObject!!.image?.let { bind.image.setImageResource(it) }
-        bind.pageName.text = splashObject!!.title
-        bind.pageDescription.text = splashObject!!.description
 
         bind.buttonSkip.setOnClickListener(this::onClick)
         bind.buttonNext.setOnClickListener(this::onClick)
@@ -34,18 +32,13 @@ class SplashPagerFragment: Fragment(), View.OnClickListener {
         return bind.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(splash: SplashData) = SplashPagerFragment().apply {
-            arguments = Bundle().apply {
-                putSerializable("splash", splash)
-            }
-        }
-    }
-
     override fun onClick(v: View?) {
         if(v == bind.buttonSkip){
             v.findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }
+
+        if (v == bind.buttonNext){
+            v.findNavController().navigate(R.id.action_splashPagerFragment_to_splashFragment2)
         }
     }
 }
