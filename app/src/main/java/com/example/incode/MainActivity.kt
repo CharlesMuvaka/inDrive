5075d92d315ad3862b3ab40e14a474fb524ebb20
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-04 12:14:59 +0300

    initialise a view model, repository and view model factory instances.

--- app/src/main/java/com/example/incode/MainActivity.kt
+++ app/src/main/java/com/example/incode/MainActivity.kt
@@ -2,31 +2,43 @@ package com.example.incode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.incode.api.RetrofitClient
+import androidx.lifecycle.ViewModelProvider
 import com.example.incode.datab.Drivers
 import com.example.incode.datab.Places
 import com.example.incode.models.Driver
 import com.example.incode.models.Place
-import com.example.incode.models.TestResultsOne
+import com.example.incode.repository.PlacesRepo
+import com.example.incode.viewmodel.PlacesModelFactory
+import com.example.incode.viewmodel.PlacesViewModel
 import com.skydoves.transformationlayout.onTransformationStartContainer
-import retrofit2.Call
-
 class MainActivity : AppCompatActivity() {
+    lateinit var placesViewModel: PlacesViewModel
     var places = ArrayList<Place>()
     var drivers = ArrayList<Driver>()
-    private lateinit var call: Call<TestResultsOne>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         onTransformationStartContainer() //initialising view transformation
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //initialising dummy search types
+        val searchType = "golf club"
+        val place = "Nairobi"
+
         places = Places().places
         drivers = Drivers().drivers
 
-        val retrofit = RetrofitClient.apiInstance
+        //initialising the places repository
+        val placesRepo = PlacesRepo()
+        // initialising the view model factory
+        val placesFactory = PlacesModelFactory(searchType, place, placesRepo)
 
-        call = retrofit.getPlace("gym", "Nairobi")
+        //initialising the view model
+        placesViewModel = ViewModelProvider(this, placesFactory)[PlacesViewModel::class.java]
+
+
+
 
     }
 }
