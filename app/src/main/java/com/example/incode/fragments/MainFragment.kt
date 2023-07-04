5c00fce4e397926b2c8a34df2e1487afdaaa03ae
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-04 12:16:02 +0300

    use the view model instance to get the data from the api.

--- app/src/main/java/com/example/incode/fragments/MainFragment.kt
+++ app/src/main/java/com/example/incode/fragments/MainFragment.kt
@@ -6,35 +6,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.Observer
 import com.example.incode.MainActivity
-import com.example.incode.SecondActivity
-import com.example.incode.adapters.PlacesAdapter
-import com.example.incode.adapters.ViewPagerAdapter
 import com.example.incode.databinding.FragmentMainBinding
-import com.example.incode.models.Place
-import com.skydoves.transformationlayout.TransformationCompat
+import com.example.incode.models.PlaceResult
+import com.example.incode.models.Resource
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-class MainFragment: Fragment(), View.OnClickListener {
+class MainFragment : Fragment(), View.OnClickListener {
+    private var placesList = ArrayList<PlaceResult>()
 
     private lateinit var bind: FragmentMainBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         bind = FragmentMainBinding.inflate(inflater)
 
-        val places = (activity as MainActivity).places
-        val adp = PlacesAdapter()
-        adp.asyncList.submitList(places)
+        //initialising the view model
+        val viewModel = (activity as MainActivity).placesViewModel
+
+        //getting the response from the view model
+        viewModel.places.observe(viewLifecycleOwner, Observer { placesDetails ->
+            when (placesDetails) {
+                is Resource.Success -> {
+                    placesDetails.let {
+                        val resultsList = it.placesData!!.results
+                        for (result in resultsList) {
+                            placesList.add(result)
+                        }
+                    }
+                }
+                is Resource.Failure -> {
+                    placesDetails.message.let {
+                        Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                    }
+                }
+                is Resource.Loading -> {
+                    Toast.makeText(requireContext(), "Loading...", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+        })
 
         //setting onclick listeners
         //bind.fab.setOnClickListener(this::onClick)
 
         return bind.root
     }
-
 
 
     override fun onClick(p0: View?) {
