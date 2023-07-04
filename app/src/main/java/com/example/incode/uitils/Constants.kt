08aebe7aafb638a2ae9539c823df1a7683539ff2
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-04 17:31:58 +0300

    shorten the google places endpoint

--- app/src/main/java/com/example/incode/uitils/Constants.kt
+++ app/src/main/java/com/example/incode/uitils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.incode.uitils
 
 object Constants {
     val yes = "AIzaSyBNkYg7q_1bdDVoVfAkicIW4GmhNdBqMwQ"
-    val BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    val BASE_URL = "https://maps.googleapis.com/maps/api/"
 }
