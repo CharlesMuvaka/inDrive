d613667c01ed35c10d01710c8fff86ffa4e89892
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-04 17:37:43 +0300

    use named variables to define the parameters to be passed

--- app/src/main/java/com/example/incode/repository/PlacesRepo.kt
+++ app/src/main/java/com/example/incode/repository/PlacesRepo.kt
@@ -3,5 +3,5 @@ package com.example.incode.repository
 import com.example.incode.api.RetrofitClient
 
 class PlacesRepo {
-    suspend fun getPlaces(type: String, place: String) = RetrofitClient.apiInstance.getPlace(type,place)
+    suspend fun getPlaces(type: String, place: String) = RetrofitClient.apiInstance.getPlace(searchType = type, placeFound = place)
 }
