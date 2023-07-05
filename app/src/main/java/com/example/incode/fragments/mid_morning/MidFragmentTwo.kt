dfa30d876699778f20acc86cea22d26b6d4dea60
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-05 16:46:16 +0300

    define an action to navigate to the next fragment.

--- app/src/main/java/com/example/incode/fragments/mid_morning/MidFragmentTwo.kt
+++ app/src/main/java/com/example/incode/fragments/mid_morning/MidFragmentTwo.kt
@@ -1,60 +1,28 @@
 package com.example.incode.fragments.mid_morning
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import com.example.incode.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MidFragmentTwo.newInstance] factory method to
- * create an instance of this fragment.
- */
+import com.example.incode.databinding.FragmentMidTwoBinding
 class MidFragmentTwo : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var bind:FragmentMidTwoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mid_two, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MidFragmentTwo.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MidFragmentTwo().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        bind= FragmentMidTwoBinding.inflate(inflater)
+        val action = MidFragmentTwoDirections.actionMidFragmentTwoToMidFragmentOne()
+        Handler(Looper.getMainLooper()).postDelayed({
+            NavHostFragment.findNavController(this).navigate(action)
+        }, 5000)
+        return bind.root
     }
 }
