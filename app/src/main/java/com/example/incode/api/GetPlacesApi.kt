deec97d011a306422a374354326bb6044910c5e1
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-11 18:58:20 +0300

    define a method to get places from the api.

--- app/src/main/java/com/example/incode/api/GetPlacesApi.kt
+++ app/src/main/java/com/example/incode/api/GetPlacesApi.kt
@@ -4,6 +4,7 @@ import com.example.incode.models.PlaceResult
 import com.example.incode.models.RouteDirections
 import com.example.incode.models.TestResultsOne
 import com.example.incode.uitils.Constants
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -18,6 +19,15 @@ interface GetPlacesApi {
         @Query("key")
         apiKey: String = Constants.yes
     ): Response<TestResultsOne>
+    @GET("place/textsearch/json?")
+    fun getPlaces(
+        @Query("query")
+        searchType: String,
+        @Query("query")
+        placeFound: String,
+        @Query("key")
+        apiKey: String = Constants.yes
+    ): Call<TestResultsOne>
 
     @GET("directions/json?")
     suspend fun getDirections(
