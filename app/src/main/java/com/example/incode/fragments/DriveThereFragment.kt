576534c53c4050c69dbbf4f69fc64e0f4792d68c
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-15 13:49:05 +0300

    bind driver and car data to the layout.

--- app/src/main/java/com/example/incode/fragments/DriveThereFragment.kt
+++ app/src/main/java/com/example/incode/fragments/DriveThereFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.incode.R
+import com.example.incode.datab.Drivers
 import com.example.incode.databinding.FragmentDriveThereBinding
 import com.example.incode.models.PlaceResult
 
@@ -26,8 +27,15 @@ class DriveThereFragment : Fragment(), View.OnClickListener {
     ): View? {
         // Inflate the layout for this fragment
         bind = FragmentDriveThereBinding.inflate(inflater)
+        var driver = Drivers().drivers[0]
+        var placeName = place!!.name.split(" ")
+        bind.title.text = "Drive to ${placeName[0]}"
+        bind.driver.text = driver.name
+        bind.car.text = "${driver.car!!.name} ${driver.car!!.numberPlate}"
+        bind.phone.text = driver.phone
 
-        bind.wallet.setOnClickListener(this::onClick)
+
+        bind.account.setOnClickListener(this::onClick)
         bind.back.setOnClickListener(this::onClick)
         return bind.root
     }
@@ -43,7 +51,7 @@ class DriveThereFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        if(v == bind.wallet){
+        if(v == bind.account){
             val intent = requireContext().packageManager.getLaunchIntentForPackage("com.android.stk")
             if (intent != null){
                 requireContext().startActivity(intent)
