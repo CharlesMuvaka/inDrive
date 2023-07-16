605a80c6c9934702569692fc908e29087e65dc75
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-16 15:29:53 +0300

    get the users location.

--- app/src/main/java/com/example/incode/fragments/BookRideFragment.kt
+++ app/src/main/java/com/example/incode/fragments/BookRideFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.incode.MainActivity
 import com.example.incode.R
 import com.example.incode.databinding.FragmentBookRideBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class BookRideFragment : Fragment(), OnMapReadyCallback, View.OnClickListener {
@@ -25,10 +29,18 @@ class BookRideFragment : Fragment(), OnMapReadyCallback, View.OnClickListener {
         val supportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         supportMapFragment.getMapAsync(this::onMapReady)
 
+        userLat = (activity as MainActivity).locationLatitude
+        userLong = (activity as MainActivity).locationLongitude
+
         return bind.root
     }
     override fun onMapReady(p0: GoogleMap) {
         googleMap = p0
+        val sydney = LatLng(userLat!!, userLong!!)
+        //googleMap.addMarker(MarkerOptions().position(sydney).title("Sidney city"))
+        //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f))
+
+
     }
 
     override fun onClick(v: View?) {
